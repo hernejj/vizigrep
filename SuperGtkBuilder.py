04d8840015559fcde1e761c60ae6fb49791f6fb2
Jason J. Herne <hernejj@gmail.com>
2013-02-27 08:02:25 -0500

    Skip widgets known to be non-buildable

--- SuperGtkBuilder.py
+++ SuperGtkBuilder.py
@@ -13,4 +13,5 @@ class SuperGtkBuilder(Gtk.Builder):
     # found in the GtkBuilder UI file.            
     def populate_window_with_ui_elements(self, window_object):
         for ui_element in self.get_objects():
+            if isinstance(ui_element, Gtk.TreeSelection): continue
             setattr(window_object, Gtk.Buildable.get_name(ui_element), ui_element)

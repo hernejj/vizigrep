c729dcb7141ca77ee149af190bd7be1234195258
Jason J. Herne <hernejj@gmail.com>
2013-03-13 21:42:36 -0400

    Update ubuntu installer to use release tgz

--- install/ubuntu-installer/make-deb.sh
+++ install/ubuntu-installer/make-deb.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-PKG_TREE_NAME="vizigrep"
 SRC_DIR="../../"
+TGZ_DIR="../"
+PROGRAM_NAME="vizigrep"
+VERSION=`cat $SRC_DIR\.version`
+PKG_TREE_NAME=$PROGRAM_NAME-$VERSION
 
-# Remove old package tree & DEB file, and create new package tree
+# Remove old package tree & DEB files
 rm -rf $PKG_TREE_NAME
-rm -f ./*.deb
-mkdir $PKG_TREE_NAME
+rm -rf ./deb
 
-# Copy source
-cp $SRC_DIR/* $PKG_TREE_NAME/
+# Generate and Unpack source
+cd $TGZ_DIR
+./make-generic-release.sh
+cd -
+tar -zxf $TGZ_DIR/$PROGRAM_NAME-$VERSION.tgz
 
-# Copy build related files
+# Inject debian build specific files
 cp Makefile $PKG_TREE_NAME/
 cp vizigrep.sh $PKG_TREE_NAME/
 cp -a debian $PKG_TREE_NAME/
@@ -24,4 +29,4 @@ cd ..
 # Cleanup
 mkdir -p  deb
 mv *.deb *.tar.gz *.dsc *.build *.changes deb/
-rm -rf $PKG_TREE_NAME
+#rm -rf $PKG_TREE_NAME

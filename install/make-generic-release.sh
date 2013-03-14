bb0b4d154c580ddc5a9c0a61df61f32e8d2324b6
Jason J. Herne <hernejj@gmail.com>
2013-03-13 21:34:21 -0400

    Append version number to directory name

--- install/make-generic-release.sh
+++ install/make-generic-release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-PKG_TREE_NAME="vizigrep"
 SRC_DIR="../"
+PROGRAM_NAME="vizigrep"
 VERSION=`cat $SRC_DIR\.version`
+PKG_TREE_NAME=$PROGRAM_NAME-$VERSION
 
 # Remove old package tree & tgz file, and create new package tree
 rm -rf $PKG_TREE_NAME
@@ -19,7 +20,7 @@ cp $SRC_DIR/vizigrep.svg $PKG_TREE_NAME/
 cat $SRC_DIR/vizigrep.man | sed s/vizigrep-[0-9]*[.][0-9]*/vizigrep-$VERSION/ > $PKG_TREE_NAME/vizigrep.man
 
 # Create tgz package
-tar -zcf $PKG_TREE_NAME-$VERSION.tgz $PKG_TREE_NAME/
+tar -zcf $PROGRAM_NAME-$VERSION.tgz $PKG_TREE_NAME/
 
 # Remove temp junk
 rm -rf $PKG_TREE_NAME

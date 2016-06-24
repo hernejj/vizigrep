ecd9d3591425ef14d392af81a3a26cd52b81dbb4
Jason J. Herne <hernejj@gmail.com>
2016-06-24 11:17:29 -0400

    Fix run to run the local code and NOT need to install

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#sudo ./setup.py install 1> /dev/null
+#sudo ./setup.py clean 1> /dev/null
+#vizigrep
+
+import vizigrep.main
+
+vizigrep.main.main()
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
-sudo ./setup.py install 1> /dev/null
-sudo ./setup.py clean 1> /dev/null
-vizigrep
+import vizigrep.main
+
+vizigrep.main.main()

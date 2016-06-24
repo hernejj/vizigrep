a2f00539ebe53bd9cd00f634748d18c5d608f8fd
Jason J. Herne <hernejj@gmail.com>
2016-06-24 11:50:06 -0400

    Fix make-test.sh to not include old debs and tgzs

--- install/ubuntu-installer/make-test-tgz.sh
+++ install/ubuntu-installer/make-test-tgz.sh
@@ -6,11 +6,15 @@ VERSION=`cat $SRC_DIR\.version`
 PKG_TREE_NAME=$PROGRAM_NAME-$VERSION
 GITHUB_TAR_NAME=v"$VERSION".tar.gz
 
-# Remove old package tree
+# Remove old package tree & tgz
 rm -rf /tmp/$PKG_TREE_NAME
+rm -f $GITHUB_TAR_NAME
 
 # Make & populate folder containing fake release
 cp -a $SRC_DIR. /tmp/$PKG_TREE_NAME # Yes, the . is important. It causes hidden files to be copied.
+
+# Remove ./deb folder from PKG_TREE if it exists - stops useless files from getting rolled up into test tgz
+rm -rf /tmp/$PKG_TREE_NAME/install/ubuntu-installer/deb
 
 # Tar.gz it!
 cd /tmp
